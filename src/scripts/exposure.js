16994fc2b20852c75028cf62e0f7462fb064b8be
jinkimdata <jinkimdata@gmail.com>
2017-03-06 17:09:17 -0500

    exposure graphic fixes

--- src/scripts/exposure.js
+++ src/scripts/exposure.js
@@ -256,41 +256,71 @@ function load_exposure_data(race) {
 	var raceText
 
 	if (race["race-dropdown"] == "All") {
-		raceText = "an average school";
 		chairColor = "grey";
+		$("#student-text").addClass('displaynone');
+		$("#student-text").removeClass('displayall');
+		$("#district-text").addClass('displayall');
 	};
 
 
 	if (race["race-dropdown"] == "White") {
 		raceText = "a white student";
 		chairColor = "yellow";
+		$("#student-text").addClass('displayall');
+		$("#district-text").addClass('displaynone');
+		$("#district-text").removeClass('displayall');
 	};
 
 	if (race["race-dropdown"] == "Hispanic") {
 		raceText = "a hispanic student";
 		chairColor = "blue";
+		$("#student-text").addClass('displayall');
+		$("#district-text").addClass('displaynone');
+		$("#district-text").removeClass('displayall');
 	};
 
 	if (race["race-dropdown"] == "Black") {
 		raceText = "a black student";
 		chairColor = "red";
+		$("#student-text").addClass('displayall');
+		$("#district-text").addClass('displaynone');
+		$("#district-text").removeClass('displayall');
 	};
 
 	if (race["race-dropdown"] == "Asian") {
 		raceText = "an asian student";
 		chairColor = "green";
+		$("#student-text").addClass('displayall');
+		$("#district-text").addClass('displaynone');
+		$("#district-text").removeClass('displayall');
 	};
 
 	if (race["race-dropdown"] == "All") {
-		exposureText = "racial makeup";
+		exposureText = "the racial makeup of the average school";
 	} else {
-		exposureText = "exposure rate";
+		exposureText = "the racial makeup of the average school";
 	}
 
+	//This formula calculates change in area
+	var exposureWhite
+	exposureWhite = Math.sqrt(((7854 * (race["exposure-white"]/100)) / 3.1416)) * 2;
+
+	var exposureBlack
+	exposureBlack = Math.sqrt(((7854 * (race["exposure-black"]/100)) / 3.1416)) * 2;
+
+	var exposureHispanic
+	exposureHispanic = Math.sqrt(((7854 * (race["exposure-hispanic"]/100)) / 3.1416)) * 2;
+
+	var exposureAsian
+	exposureAsian = Math.sqrt(((7854 * (race["exposure-asian"]/100)) / 3.1416)) * 2;
+
+	var exposureOther
+	exposureOther = Math.sqrt(((7854 * (race["exposure-other"]/100)) / 3.1416)) * 2;
 
 
 	$("#text-exposure").html(exposureText);
 	$("#text-district").html(race["district"]);
+	$("#text-district2").html(race["district"]);
 	$("#text-race").html(raceText);
 	$("#chair-svg").removeClass();
 	$("#chair-svg").addClass(chairColor);
@@ -299,28 +329,28 @@ function load_exposure_data(race) {
 
 	$("#asian-text").html(race["exposure-asian"] + "%");
 	$("#asian-dif").html(race["asian-dif"] );
-	$("#asian-circle").css( "width", race["exposure-asian"] );
-	$("#asian-circle").css( "height", race["exposure-asian"] );
+	$("#asian-circle").css( "width", exposureAsian );
+	$("#asian-circle").css( "height", exposureAsian );
 
 	$("#white-text").html(race["exposure-white"] + "%");
 	$("#white-dif").html(race["white-dif"]);
-	$("#white-circle").css( "width", race["exposure-white"] );
-	$("#white-circle").css( "height", race["exposure-white"] );
+	$("#white-circle").css( "width", exposureWhite );
+	$("#white-circle").css( "height", exposureWhite );
 
 	$("#black-text").html(race["exposure-black"] + "%");
 	$("#black-dif").html(race["black-dif"] );
-	$("#black-circle").css( "width", race["exposure-black"] );
-	$("#black-circle").css( "height", race["exposure-black"] );
+	$("#black-circle").css( "width", exposureBlack );
+	$("#black-circle").css( "height", exposureBlack );
 
 	$("#hispanic-text").html(race["exposure-hispanic"] + "%");
 	$("#hispanic-dif").html(race["hispanic-dif"]);
-	$("#hispanic-circle").css( "width", race["exposure-hispanic"] );
-	$("#hispanic-circle").css( "height", race["exposure-hispanic"] );
+	$("#hispanic-circle").css( "width", exposureHispanic );
+	$("#hispanic-circle").css( "height", exposureHispanic );
 
 	$("#other-text").html(race["exposure-other"] + "%");
 	$("#other-dif").html(race["other-dif"] );
-	$("#other-circle").css( "width", race["exposure-other"] );
-	$("#other-circle").css( "height", race["exposure-other"] );
+	$("#other-circle").css( "width", exposureOther );
+	$("#other-circle").css( "height", exposureOther );
 
 };
 
@@ -337,30 +367,39 @@ load_poverty_exposure_data(frmp)
 function load_poverty_exposure_data(frmp) {
 
 	if (frmp["poverty-dropdown"] == "All") {
-		frmpText = "an average school";
 		frmpChairColor = "grey";
+		$("#student-text-poverty").addClass('displaynone');
+		$("#student-text-poverty").removeClass('displayall');
+		$("#district-text-poverty").addClass('displayall');
 	};
 
 	if (frmp["poverty-dropdown"] == "Reduced") {
 		frmpText = "a reduced student";
 		frmpChairColor = "red";
+		$("#student-text-poverty").addClass('displayall');
+		$("#district-text-poverty").addClass('displaynone');
+		$("#district-text-poverty").removeClass('displayall');
 	};
 
 	if (frmp["poverty-dropdown"] == "Non-Reduced") {
 		frmpText = "a non-reduced student";
 		frmpChairColor = "blue";
+		$("#student-text-poverty").addClass('displayall');
+		$("#district-text-poverty").addClass('displaynone');
+		$("#district-text-poverty").removeClass('displayall');
 	};
 
-	if (frmp["poverty-dropdown"] == "All") {
-		frmpExposureText = "poverty makeup";
-	} else {
-		frmpExposureText = "poverty exposure";
-	};	
+
+	//This formula calculates change in area
+	var exposurePovertyReduced
+	exposurePovertyReduced = Math.sqrt(((7854 * (frmp["exposure-reduced"]/100)) / 3.1416)) * 2;
+
+	var exposurePovertyNonReduced
+	exposurePovertyNonReduced = Math.sqrt(((7854 * (frmp["exposure-non-reduced"]/100)) / 3.1416)) * 2;
 
 
-	$("#text-poverty-exposure").html(frmpExposureText);
 	$("#text-poverty-district").html(frmp["district"]);
-	$("#text-poverty-rate").html(frmpText);
+	$("#text-poverty-district2").html(frmp["district"]);
 	$("#chair-svg-poverty").removeClass();
 	$("#chair-svg-poverty").addClass( frmpChairColor );
 	$("#chair-svg-poverty-bottom").removeClass();
@@ -368,13 +407,13 @@ function load_poverty_exposure_data(frmp) {
 
 	$("#reduced-text").html(frmp["exposure-reduced"] + "%");
 	$("#reduced-dif").html(frmp["reduced-dif"] );
-	$("#reduced-circle").css( "width", frmp["exposure-reduced"] );
-	$("#reduced-circle").css( "height", frmp["exposure-reduced"] );
+	$("#reduced-circle").css( "width", exposurePovertyReduced );
+	$("#reduced-circle").css( "height", exposurePovertyReduced );
 
 	$("#non-reduced-text").html(frmp["exposure-non-reduced"] + "%");
 	$("#non-reduced-dif").html(frmp["non-reduced-dif"] );
-	$("#non-reduced-circle").css( "width", frmp["exposure-non-reduced"] );
-	$("#non-reduced-circle").css( "height", frmp["exposure-non-reduced"] );
+	$("#non-reduced-circle").css( "width", exposurePovertyNonReduced );
+	$("#non-reduced-circle").css( "height", exposurePovertyNonReduced );
 
 };
 
