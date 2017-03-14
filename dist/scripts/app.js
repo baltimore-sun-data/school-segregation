8683489a3a62414bd6d3ac104c3aee383d9d86a8
jinkimdata <jinkimdata@gmail.com>
2017-03-14 14:49:47 -0400

    minor updates

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1033,7 +1033,7 @@ var app = app || {
     },
     share: function() {
         $(".icon-twitter").on("click", function() {
-            var tweet = "";
+            var tweet = "The struggle to move past segregated schools.";
             var url = "http://data.baltimoresun.com/news/bridging-the-divide/";
             var twitter_url = "https://twitter.com/intent/tweet?text=" + tweet + "&url=" + url + "&tw_p=tweetbutton";
             window.open(twitter_url, "mywin", "left=200,top=200,width=500,height=300,toolbar=1,resizable=0");
@@ -1042,7 +1042,7 @@ var app = app || {
         $(".icon-facebook").on("click", function() {
             var picture = "http://data.baltimoresun.com/news/bridging-the-divide/images/thumb-fb.png";
             var title = "Bridging the Divide";
-            var description = "";
+            var description = "The struggle to move past segregated schools.";
             var url = "http://data.baltimoresun.com/news/bridging-the-divide/";
             var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link=" + url + "&picture=" + picture + "&name=" + title + "&description=" + description + "&redirect_uri=http://www.facebook.com";
             window.open(facebook_url, "mywin", "left=200,top=200,width=500,height=300,toolbar=1,resizable=0");
--- src/scripts/javascript.js
+++ src/scripts/javascript.js
@@ -12,7 +12,7 @@ var app = app || {
 	},
 	share: function(){
 		$(".icon-twitter").on("click", function(){
-			var tweet = ""; //Tweet text
+			var tweet = "The struggle to move past segregated schools."; //Tweet text
 			var url = "http://data.baltimoresun.com/news/bridging-the-divide/"; //Interactive URL
 			var twitter_url = "https://twitter.com/intent/tweet?text="+tweet+"&url="+url+"&tw_p=tweetbutton";
 			window.open(twitter_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
@@ -20,7 +20,7 @@ var app = app || {
 		$(".icon-facebook").on("click", function(){
 			var picture = "http://data.baltimoresun.com/news/bridging-the-divide/images/thumb-fb.png"; //Picture URL
 			var title = "Bridging the Divide"; //Post title
-			var description = ""; //Post description
+			var description = "The struggle to move past segregated schools."; //Post description
 			var url = "http://data.baltimoresun.com/news/bridging-the-divide/"; //Interactive URL
 			var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link="+url+"&picture="+picture+"&name="+title+"&description="+description+"&redirect_uri=http://www.facebook.com";    		
 			window.open(facebook_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
